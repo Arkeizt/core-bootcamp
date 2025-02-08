f752fd5fcdec0ef96d4113c296ac9a4b36fdb76e
Fionnah Keiz Coyoca <f.coyoca.545499@umindanao.edu.ph>
2025-02-08 16:46:39 +0800

    yippeee nft

--- 04-erc721/contracts/MyNFT.sol
+++ 04-erc721/contracts/MyNFT.sol
@@ -6,17 +6,17 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT is ERC721URIStorage {
-    uint256 tokenId;
+contract disnaughts is ERC721URIStorage {
+    uint256 tokenId = 1;
 
-    constructor() ERC721("MyERC721Token", "MET") {}
+    constructor() ERC721("disnaughts", "DSN") {}
 
     function mint() external {
-        // _safeMint(msg.sender, tokenId);
-        // _setTokenURI(
-        //     tokenId,
-        //     "https://path/to/metadata-uri"
-        // );
-        // tokenId++;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(
+            tokenId,
+            "https://gist.githubusercontent.com/Arkeizt/875d056c0427d2b228411ff2e4976cfd/raw/1bd3dc1c5e50d57d0f3b96c378d0f6414304ebe3/metadata.json"
+        );
+        tokenId++;
     }
 }
